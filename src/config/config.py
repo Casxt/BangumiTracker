dd15f4d1b622c72b382924dabe8cc50a894054f1
casxt <maple@forer.cn>
2024-03-10 18:07:37 +0800

    update

--- src/config/config.py
+++ src/config/config.py
@@ -6,5 +6,5 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=('.env.dev', '.env.prod', '.env'), env_file_encoding='utf-8')
 
-    share_dmhy_org_tarcker_config_path: str = "storage/share_dmhy_org_tracker/config.json"
+    share_dmhy_org_tarcker_config_path: str = "storage/tracker/share_dmhy_org.json"
     bangumi_data_dir: str = "storage/bangumi"

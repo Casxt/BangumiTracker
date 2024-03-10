b5a84be543485028d28fdc33bf037150420f7221
casxt <maple@forer.cn>
2024-03-10 17:20:19 +0800

    update script

--- generage_proto.sh
+++ generage_proto.sh
@@ -13,8 +13,8 @@ done
 for dir_path in $(find $OUT_DIR -mindepth 1 -maxdepth 1 -type d); do
     package=$(basename "$dir_path")
     echo "Found package ${package} Beginning replacement"
-    find $OUT_DIR/. -name '*.py' -exec sed -i '' "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
-    find $OUT_DIR/. -name '*.pyi' -exec sed -i '' "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+    find $OUT_DIR/. -name '*.py' -exec sed -i '' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+    find $OUT_DIR/. -name '*.pyi' -exec sed -i '' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
 done
 
 tree $OUT_DIR

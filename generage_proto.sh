b1932f99ea501546b6ad58d37208240985e150ee
casxt <maple@forer.cn>
2024-03-10 17:31:23 +0800

    update

--- generage_proto.sh
+++ generage_proto.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+os_name=$(uname -s)
+
+is_darwin=0
+if [ "$os_name" = "Darwin" ]; then
+    is_darwin=1
+else
+    is_darwin=0
+fi
+
 cd proto
 
 PY_PKG=proto_py
@@ -13,8 +23,14 @@ done
 for dir_path in $(find $OUT_DIR -mindepth 1 -maxdepth 1 -type d); do
     package=$(basename "$dir_path")
     echo "Found package ${package} Beginning replacement"
-    find $OUT_DIR/. -name '*.py' -exec sed -i '.bak' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
-    find $OUT_DIR/. -name '*.pyi' -exec sed -i '.bak' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+    if [ "$os_name" = "Darwin" ]; then
+      find $OUT_DIR/. -name '*.py' -exec sed -i '' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+      find $OUT_DIR/. -name '*.pyi' -exec sed -i '' -e "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+    else
+      find $OUT_DIR/. -name '*.py' -exec sed -i "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+      find $OUT_DIR/. -name '*.pyi' -exec sed -i "s#from ${package}#from ${PY_PKG}.${package}#g" {} \;
+    fi
+
 done
 
 tree $OUT_DIR
